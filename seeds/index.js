164ea2348c2222686fbee268e76b780ae66432d2
Jared Elliott <bootcamp@jaredclt.com>
2024-01-31 18:18:15 -0500

    Seed data structure and database schema.

---
+++ seeds/index.js
@@ -0,0 +1,4 @@
+const sequelize = require('../config/connection');
+const seedUser = require('./userData.js');
+const seedPost = require('./postData.js');
+const seedComment = require('./commentData.js');
