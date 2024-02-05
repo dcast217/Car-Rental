9ab885253bb9145b8019b92f621710da8d536be2
dcast217 <151418582+dcast217@users.noreply.github.com>
2024-02-05 10:06:37 -0500

    updated controllers

--- controllers/index.js
+++ controllers/index.js
@@ -9,12 +9,30 @@ router.get('/profile', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    res.render('home', {layout: 'main'});
+    res.render('home', {layout: 'hero'});
 });
 
 // Catch all for routing
 router.get('*', (req, res) => {
     res.status(404).json({message: 'Invalid route'})
+});
+// Configure Handlebars as the view engine
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+// Route to render the login form
+app.get('/login', (req, res) => {
+  res.render('form', {form: 'login', layout: 'main'});
+});
+
+// Handle form submission
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  
+  // Perform authentication logic here
+  
+  // Redirect to a different page after successful login
+  res.render('form', {form: 'signup', layout: 'main'});
 });
 
 module.exports = router;
